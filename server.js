371db20ccbbe7812719d79f7949ea3dcffb54c9e
oyewale tayo <tayooyewale@gmail.com>
2020-01-11 08:55:11 +0100

    changing response

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ database : 'smartbrain'
 const app = express(); 
 app.use(bodyParser.json())
 app.use(cors())
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> { res.send('its working') })
 app.post('/signin', (req, res) =>{signin.handleSignin(req,res,db,bcrypt)})
 app.post('/register', (req, res)=>{register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) =>{profile.handleProfileGet(req, res, db)})
