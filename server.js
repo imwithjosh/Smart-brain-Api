426cd518377d1c241f8b66b016a7a0395ad34fcc
oyewale tayo <tayooyewale@gmail.com>
2020-01-11 09:37:53 +0100

    reverting to start up server

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ database : 'smartbrain'
 const app = express(); 
 app.use(bodyParser.json())
 app.use(cors())
-app.get('/', (req, res)=> { res.send('its working') })
+app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', (req, res) =>{signin.handleSignin(req,res,db,bcrypt)})
 app.post('/register', (req, res)=>{register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) =>{profile.handleProfileGet(req, res, db)})
@@ -28,7 +28,7 @@ app.put('/image', (req, res) =>{image.handleImage(req, res,db)})
 app.post('/imageurl', (req, res) =>{image.handleApiCall(req, res)})
 
 
-app.listen(process.env.PORT || 3000, ()=>{
-console.log(`app is running on port ${process.env.PORT}`);
+app.listen(3000, ()=>{
+console.log('app is running on port 3000');
 
 })
